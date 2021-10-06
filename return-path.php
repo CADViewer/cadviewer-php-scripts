ad0dbfe1abd5ae61d92a3a93f5a53f75848b0148
cadviewer <61973235+CADViewer@users.noreply.github.com>
2021-10-06 08:15:08 +0200

    Update return-path.php

--- return-path.php
+++ return-path.php
@@ -1,7 +1,53 @@
 <?php
 	require 'CADViewer_config.php';
 
-	echo  $home_dir;
+	$returnpathname = "";
+	
+	try {
+		if (isset($_POST['returnpathname'])){
+			$returnpathname = $_POST['returnpathname'];
+		}
+	} catch (Exception $e) {
+	}
+	
+
+	// return path as home dir , this is the default
+	if ($returnpathname == "getServerLocationFromScript" || $returnpathname == "getServerLocationFromScript_00"){
+		echo  $home_dir;
+		exit;
+	}
+
+
+	// add in any custom path... 
+	if ($returnpathname == "getServerLocationFromScript_01"){
+		echo  $myCustomPath01;
+		exit;
+	}
+
+
+	// Redlines 
+	if ($returnpathname == "getServerLocationFromScript_02"){
+		echo  $home_dir. "/content/redlines/api_650/";
+		exit;
+	}
+	
+
+	// FileFolder 
+	if ($returnpathname == "getServerLocationFromScript_03"){
+		echo  $home_dir. "/content/drawings/dwg/";
+		exit;
+	}
+
+	
+	// FileFolder 
+	if ($returnpathname == "getServerLocationFromScript_04"){
+		echo  $httpHost . "/content/drawings/dwg/";
+		exit;
+	}
+
+
+
+	echo "noPathSpecifiedOnServer";
 
 exit;
 ?>
